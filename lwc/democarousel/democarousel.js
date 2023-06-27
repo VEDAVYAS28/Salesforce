028d3251445a6c70c06fe5c023c8d81c19daca48
VEDAVYAS28 <>
2023-06-27 09:15:34 +0000

    [ClickDeploy] Dev to Github

---
+++ lwc/democarousel/democarousel.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import IMAGES from "@salesforce/resourceUrl/cars";
+
+export default class Basic extends LightningElement {
+    cars1=IMAGES + '/18015-MC20BluInfinito-scaled-e1666008987698.jpg'
+    cars2=IMAGES + '/exterior-right-front-three-quarter-2.jpeg'
+    cars3=IMAGES + '/fastest-cars-world-2021-luxe-digital@2x.jpeg'
+}
